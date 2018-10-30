682f8ce5ba6e204901c48aa8aac3e691dc7e52e0
Ian Turgeon <iturgeon@gmail.com>
2018-10-30 19:56:41 -0400

    update mwdk, package.json, move included deps into npm

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,22 @@
+const path = require('path')
+const widgetWebpack = require('materia-widget-development-kit/webpack-widget')
+
+const copy = widgetWebpack.getDefaultCopyList()
+
+const customCopy = copy.concat([
+	{
+		from: path.join(__dirname, 'node_modules', 'angular-chart.js', 'dist', 'angular-chart.min.js'),
+		to: path.join(__dirname, 'build', 'vendor')
+	},
+	{
+		from: path.join(__dirname, 'node_modules', 'angular-chart.js', 'dist', 'angular-chart.min.css'),
+		to: path.join(__dirname, 'build', 'vendor')
+	},
+])
+
+let options = {
+	copyList: customCopy
+}
+
 // load the reusable legacy webpack config from materia-widget-dev
-module.exports = require('materia-widget-development-kit/webpack-widget').getLegacyWidgetBuildConfig()
+module.exports =  widgetWebpack.getLegacyWidgetBuildConfig(options)
